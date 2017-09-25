99891aabb7790b50149f3f8f78e846ef9f23f95e
sandeepc <sandeep.chauhan@theoptimum.net>
2017-09-25 23:31:32 +0800

    [sndy] change urlLogic for app deployment

--- src/js/services/VODService.js
+++ src/js/services/VODService.js
@@ -2,14 +2,15 @@ import axios from 'axios';
 
 export default class VODService {
 	constructor(endpointUrl) {
-		this.endpointUrl = endpointUrl;
+		this.endpointUrl = 'http://demo2697834.mockable.io/movies';
 	}
 	getData() {
 		return axios.get(this.endpointUrl)
-		.then(response =>
-			response.status === 200 ? JSON.parse(response.data).entries : []
-		)
-		.catch(error =>
+		.then(response => {
+			//added to format data between localhost & direct url call
+			return response.status !== 200 ? [] : typeof response.data === 'string' ? 
+			JSON.parse(response.data).entries : JSON.parse(JSON.stringify(response.data)).entries;
+		}).catch(error =>
 			console.log(error)
 		);
 	}
